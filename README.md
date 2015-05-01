1e2f55772c7429759609faac56bd9404b0c7dc0c
Lasse Bunk <lasse@bunk.io>
2015-05-01 17:23:57 +0200

    Initialcommit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Screenshot Generator
+
+Screenshot Generator is a WordPress plugin that takes screenshots posts for social media etc. when they are updated.
+
+It uses [PhantomJS](http://phantomjs.org/) to do this.
+
+The screenshot will look something like this when shared on social media:
+
+![Screenshot]()
+
+## Installation
+
+1. Download the plugin to `wp-content/plugins`.
+2. Install PhantomJS.
+   * **Mac:**
+
+     ```bash
+     brew install phantomjs
+     ```
+
+     Or if this fails, [download from here](https://github.com/eugene1g/phantomjs/releases)
+
+   * **Linux:**
+
+     ```bash
+     $ apt-get install phantomjs fontconfig freetype*
+     ```
+3. Make sure the `phantomjs` binary is in your PHP's `PATH`.
+4. Update a post.
+
+## Usage
+
+When you update a post, a screenshot is automatically taken in the background.
+After a few seconds, you can retrieve the screenshot from the post's meta key
+`_scrgen_screenshot`.
+
+An `og:image` and `twitter:image:src` is automatically added to the meta tags
+when viewing the post. These are only added if the post doesn't have a
+thumbnail, in which case the plugin expects the thumbnail to be added by
+you or another plugin.
+
+## Credits
+
+The screenshot functionality is adapted from Microweber's
+[Screen](https://github.com/microweber/screen) code.
+
+## Contributing
+
+Contributions are very welcome as I'm not very experienced in developing
+WordPress plugins. Please contribute!
---
+++ screenshot-generator.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Plugin Name: Screenshot Generator
+ * Plugin URI: http://lassebunk.dk/plugins/screenshot-generator
+ * Description: Take automatic screenshots of posts when they are updated.
+ * Version: 0.0.1
+ * Author: Lasse Bunk
+ * Author URI: http://lassebunk.dk
+ * License: MIT
+ */
+
+function scrgen_generate_post_screenshot($post_id) {
+  // Workaround bug: http://premium.wpmudev.org/forums/topic/snapshot-faltal-error-on-get_home_path
+  if (!function_exists('get_home_path' )) require_once( dirname(__FILE__) . '/../../../wp-admin/includes/file.php' );
+
+  $url = get_permalink($post_id);
+  $url_segs = parse_url($url);
+
+  $relative_path = 'wp-content/screenshots/' . $post_id . '.jpg';
+  $image_path = get_home_path() . $relative_path;
+  $image_url = get_home_url() . '/' . $relative_path;
+
+  $job_id = uniqid();
+  $job_path = "/tmp/{$job_id}.js"; //tempnam(sys_get_temp_dir(), 'scrgen');
+  $tmp_path = "/tmp/{$job_id}.jpg";
+
+  $w = 1024;
+  $h = 768;
+  $clipw = 1024;
+  $cliph = 640;
+
+  $url = strip_tags($url);
+  $url = str_replace(';', '', $url);
+  $url = str_replace('"', '', $url);
+  $url = str_replace('\'', '/', $url);
+  $url = str_replace('<?', '', $url);
+  $url = str_replace('<?', '', $url);
+  $url = str_replace('\077', ' ', $url);
+
+  $url = escapeshellcmd($url);
+  $src = "
+
+  var page = require('webpage').create();
+
+  page.viewportSize = { width: {$w}, height: {$h} };
+
+  ";
+
+  if (isset($clipw) && isset($cliph)) {
+      $src .= "page.clipRect = { top: 0, left: 0, width: {$clipw}, height: {$cliph} };";
+  }
+
+  $src .= "
+
+  page.open('{$url}', function () {
+      page.render('{$tmp_path}');
+      phantom.exit();
+  });
+
+
+  ";
+
+  file_put_contents($job_path, $src);
+
+  $exec = PHANTOMJS . ' ' . $job_path;
+  $escaped_command = escapeshellcmd($exec);
+
+  exec($escaped_command);
+
+  if (is_file($tmp_path)) {
+      rename($tmp_path, $image_path);
+      do_action('scrgen_post_screenshot_generated', $post_id, $image_url);
+  }
+}
+
+function scrgen_queue_post_update($post_id) {
+  wp_schedule_single_event(time(), 'scrgen_update_post_screenshot', array($post_id));
+}
+
+function scrgen_update_post_meta($post_id, $screenshot_url) {
+  update_post_meta($post_id, '_scrgen_screenshot', $screenshot_url);
+}
+
+function scrgen_screenshot() {
+  $post_id = get_the_ID();
+  $screenshot = get_post_meta($post_id, '_scrgen_screenshot', true);
+  return $screenshot;
+}
+
+function scrgen_screenshot_if_needed() {
+  $thumbnail_id = get_post_thumbnail_id();
+  $screenshot = scrgen_screenshot();
+
+  if (empty($thumbnail_id) && !empty($screenshot)) {
+    return $screenshot;
+  }
+}
+
+function scrgen_opengraph($original) {
+  $screenshot = scrgen_screenshot_if_needed();
+  if (!empty($screenshot)) {
+    echo '<meta property="og:image" content="' . $screenshot . '" />';
+  }
+}
+
+function scrgen_twitter() {
+  $screenshot = scrgen_screenshot_if_needed();
+  if (!empty($screenshot)) {
+    echo '<meta name="twitter:image:src" content="' . $screenshot . '"/>';
+  }
+}
+
+add_action('scrgen_update_post_screenshot', 'scrgen_generate_post_screenshot');
+add_action('scrgen_post_screenshot_generated', 'scrgen_update_post_meta', 10, 2);
+add_action('post_updated', 'scrgen_queue_post_update');
+
+// Automatically add screenshots when using Yoast's SEO plugin
+add_action('wp_head', 'scrgen_opengraph', 1);
+add_action('wp_head', 'scrgen_twitter', 1);
