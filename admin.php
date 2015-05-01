5fa5bd856820290954a60576ea2fecb3b46ffd11
Lasse Bunk <lasse@bunk.io>
2015-05-02 00:06:13 +0200

    Remove whitespace

--- admin.php
+++ admin.php
@@ -3,14 +3,14 @@ add_action( 'admin_menu', 'scrgen_add_admin_menu' );
 add_action( 'admin_init', 'scrgen_settings_init' );
 
 
-function scrgen_add_admin_menu(  ) { 
+function scrgen_add_admin_menu() {
 
   add_options_page( 'Screenshot Generator', 'Screenshot Generator', 'manage_options', 'screenshot-generator', 'scrgen_options_page' );
 
 }
 
 
-function scrgen_settings_init(  ) { 
+function scrgen_settings_init() {
 
   register_setting( 'scrgen-settings', 'scrgen_width' );
   register_setting( 'scrgen-settings', 'scrgen_height' );
@@ -24,120 +24,120 @@ function scrgen_settings_init(  ) {
   // Size section
 
   add_settings_section(
-    'scrgen_size_section', 
-    __( 'Screenshot size', 'scrgen' ), 
-    '', 
+    'scrgen_size_section',
+    __( 'Screenshot size', 'scrgen' ),
+    '',
     'scrgen-settings'
   );
 
-  add_settings_field( 
-    'scrgen_width', 
-    __( 'Width', 'scrgen' ), 
-    'scrgen_width_render', 
-    'scrgen-settings', 
-    'scrgen_size_section' 
+  add_settings_field(
+    'scrgen_width',
+    __( 'Width', 'scrgen' ),
+    'scrgen_width_render',
+    'scrgen-settings',
+    'scrgen_size_section'
   );
 
-  add_settings_field( 
-    'scrgen_height', 
-    __( 'Height', 'scrgen' ), 
-    'scrgen_height_render', 
-    'scrgen-settings', 
-    'scrgen_size_section' 
+  add_settings_field(
+    'scrgen_height',
+    __( 'Height', 'scrgen' ),
+    'scrgen_height_render',
+    'scrgen-settings',
+    'scrgen_size_section'
   );
 
   // Cropping section
 
   add_settings_section(
-    'scrgen_cropping_section', 
-    __( 'Cropping', 'scrgen' ), 
-    '', 
+    'scrgen_cropping_section',
+    __( 'Cropping', 'scrgen' ),
+    '',
     'scrgen-settings'
   );
 
-  add_settings_field( 
-    'scrgen_enable_cropping', 
-    __( 'Enable cropping', 'scrgen' ), 
-    'scrgen_enable_cropping_render', 
-    'scrgen-settings', 
-    'scrgen_cropping_section' 
+  add_settings_field(
+    'scrgen_enable_cropping',
+    __( 'Enable cropping', 'scrgen' ),
+    'scrgen_enable_cropping_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
   );
 
-  add_settings_field( 
-    'scrgen_crop_left', 
-    __( 'Left', 'scrgen' ), 
-    'scrgen_crop_left_render', 
-    'scrgen-settings', 
-    'scrgen_cropping_section' 
+  add_settings_field(
+    'scrgen_crop_left',
+    __( 'Left', 'scrgen' ),
+    'scrgen_crop_left_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
   );
 
-  add_settings_field( 
-    'scrgen_crop_top', 
-    __( 'Top', 'scrgen' ), 
-    'scrgen_crop_top_render', 
-    'scrgen-settings', 
-    'scrgen_cropping_section' 
+  add_settings_field(
+    'scrgen_crop_top',
+    __( 'Top', 'scrgen' ),
+    'scrgen_crop_top_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
   );
 
-  add_settings_field( 
-    'scrgen_crop_width', 
-    __( 'Width', 'scrgen' ), 
-    'scrgen_crop_width_render', 
-    'scrgen-settings', 
-    'scrgen_cropping_section' 
+  add_settings_field(
+    'scrgen_crop_width',
+    __( 'Width', 'scrgen' ),
+    'scrgen_crop_width_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
   );
 
-  add_settings_field( 
-    'scrgen_crop_height', 
-    __( 'Height', 'scrgen' ), 
-    'scrgen_crop_height_render', 
-    'scrgen-settings', 
-    'scrgen_cropping_section' 
+  add_settings_field(
+    'scrgen_crop_height',
+    __( 'Height', 'scrgen' ),
+    'scrgen_crop_height_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
   );
 
   // Social section
 
   add_settings_section(
-    'scrgen_social_section', 
-    __( 'Social settings', 'scrgen' ), 
-    'scrgen_social_section', 
+    'scrgen_social_section',
+    __( 'Social settings', 'scrgen' ),
+    'scrgen_social_section',
     'scrgen-settings'
   );
 
-  add_settings_field( 
-    'scrgen_social_strategy', 
-    __( 'Enable social screenshots', 'scrgen' ), 
-    'scrgen_social_strategy_render', 
-    'scrgen-settings', 
-    'scrgen_social_section' 
+  add_settings_field(
+    'scrgen_social_strategy',
+    __( 'Enable social screenshots', 'scrgen' ),
+    'scrgen_social_strategy_render',
+    'scrgen-settings',
+    'scrgen_social_section'
   );
 
   // Manual section
 
   add_settings_section(
-    'scrgen_manual_section', 
-    __( 'Displaying screenshots manually', 'scrgen' ), 
-    'scrgen_manual_section', 
+    'scrgen_manual_section',
+    __( 'Displaying screenshots manually', 'scrgen' ),
+    'scrgen_manual_section',
     'scrgen-settings'
   );
 
 }
 
 
-function scrgen_width_render(  ) { 
+function scrgen_width_render() {
   ?>
   <input type='number' name='scrgen_width' value='<?php echo scrgen_setting('width'); ?>'> px
   <?php
 }
 
 
-function scrgen_height_render(  ) { 
+function scrgen_height_render() {
   ?>
   <input type='number' name='scrgen_height' value='<?php echo scrgen_setting('height'); ?>'> px
   <?php
 }
 
-function scrgen_enable_cropping_render(  ) { 
+function scrgen_enable_cropping_render() {
   ?>
   <label>
     <input type='checkbox' name='scrgen_enable_cropping' <?php checked(scrgen_setting('enable_cropping'), 1); ?> value='1'>
@@ -147,35 +147,35 @@ function scrgen_enable_cropping_render(  ) {
 }
 
 
-function scrgen_crop_left_render(  ) { 
+function scrgen_crop_left_render() {
   ?>
   <input type='number' name='scrgen_crop_left' value='<?php echo scrgen_setting('crop_left'); ?>'> px
   <?php
 }
 
 
-function scrgen_crop_top_render(  ) { 
+function scrgen_crop_top_render() {
   ?>
   <input type='number' name='scrgen_crop_top' value='<?php echo scrgen_setting('crop_top'); ?>'> px
   <?php
 }
 
 
-function scrgen_crop_width_render(  ) { 
+function scrgen_crop_width_render() {
   ?>
   <input type='number' name='scrgen_crop_width' value='<?php echo scrgen_setting('crop_width'); ?>'> px
   <?php
 }
 
 
-function scrgen_crop_height_render(  ) { 
+function scrgen_crop_height_render() {
   ?>
   <input type='number' name='scrgen_crop_height' value='<?php echo scrgen_setting('crop_height'); ?>'> px
   <?php
 }
 
 
-function scrgen_social_strategy_render(  ) { 
+function scrgen_social_strategy_render() {
   ?>
   <label>
     <input type='radio' name='scrgen_social_strategy' <?php checked(scrgen_setting('social_strategy'), 'missing'); ?> value='missing'>
@@ -200,18 +200,18 @@ function scrgen_social_section() {
 }
 
 
-function scrgen_manual_section(  ) { 
+function scrgen_manual_section() {
 
   echo __( 'You can display screenshots manually inside posts by inserting: <pre>&lt;?php echo scrgen_screenshot(); ?&gt;</pre>', 'scrgen' );
 
 }
 
 
-function scrgen_options_page(  ) { 
+function scrgen_options_page() {
 
   ?>
   <form action='options.php' method='post'>
-    
+   
     <h2>Screenshot Generator</h2>
 
     <p>
@@ -231,10 +231,8 @@ function scrgen_options_page(  ) {
     do_settings_sections( 'scrgen-settings' );
     submit_button();
     ?>
-    
+   
   </form>
   <?php
 
 }
-
-?>
