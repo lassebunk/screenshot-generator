a443173fe11d759687af17c6028fd8fae4cb6ce8
Lasse Bunk <lasse@bunk.io>
2015-05-04 21:34:40 +0200

    Bump version

--- screenshot-generator.php
+++ screenshot-generator.php
@@ -3,7 +3,7 @@
  * Plugin Name: Screenshot Generator
  * Plugin URI: https://github.com/lassebunk/screenshot-generator
  * Description: Take automatic screenshots of posts when they are updated.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Lasse Bunk
  * Author URI: https://github.com/lassebunk
  * License: GPLv2 or later
