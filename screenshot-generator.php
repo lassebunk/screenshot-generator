aeedf23c9042fb4179f618f7d2590cdc449457cb
Lasse Bunk <lasse@bunk.io>
2015-05-04 22:05:40 +0200

    Check for PhantomJS constant before finding

--- screenshot-generator.php
+++ screenshot-generator.php
@@ -110,10 +110,10 @@ function scrgen_update_post_meta($post_id, $screenshot_url) {
 function scrgen_phantomjs() {
   $path = exec('which phantomjs');
 
-  if (!empty($path)) {
-    return $path;
-  } elseif (defined('PHANTOMJS')) {
+  if (defined('PHANTOMJS')) {
     return PHANTOMJS;
+  } elseif (!empty($path)) {
+    return $path;
   }
 }
 
